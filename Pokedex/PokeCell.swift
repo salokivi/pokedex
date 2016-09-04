184075f1a841f6b20a641d6f414d4ab92619af8c
Elias Salokivi <salokive@Glittertind.local>
2016-09-04 23:56:46 +0300

    csv parsing for pokemon name and id for the collectionView

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Elias Salokivi on 4.9.2016.
+//  Copyright © 2016 Elias Salokivi. All rights reserved.
+//
+
+import Cocoa
+
+class PokeCell: UICollectionViewCell {
+
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Elias Salokivi on 4.9.2016.
+//  Copyright © 2016 Elias Salokivi. All rights reserved.
+//
+
+import Foundation
