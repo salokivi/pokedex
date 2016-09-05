04d5156e92b738505b67a9e341d538c940cb701b
Elias Salokivi <salokive@Glittertind.local>
2016-09-05 22:56:48 +0300

    created PokemonDetailVC and tested segue

---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Elias Salokivi on 5.9.2016.
+//  Copyright © 2016 Elias Salokivi. All rights reserved.
+//
+
+import Cocoa
+
+class PokemonDetailVC: UIViewController {
+
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     
     var pokemon = [Pokemon]()
+    var filteredPokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
+    var inSearchMode = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collection.delegate = self
         collection.dataSource = self
-        
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done // hitting return in the searchBar will hide the keyboard
+        initAudio()
         parsePokemonCSV()
+    }
+    
+    func initAudio() {
+    
+            let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path!)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
     
     func parsePokemonCSV() {
@@ -46,7 +68,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
             
             //var pokemon = Pokemon(name: "Test", pokedexId: indexPath.row)
-            let poke = pokemon[indexPath.row] // get a pokemon from the pokemon-array
+            //let poke = pokemon[indexPath.row] // get a pokemon from the pokemon-array
+            
+            let poke: Pokemon!
+            
+            if inSearchMode {
+                poke = filteredPokemon[indexPath.row] // if we are searching, grab it from the filtered list
+            } else {
+                poke = pokemon[indexPath.row]       // if not searching, grab from the normal list
+            }
+            
             cell.configureCell(poke)
             return cell
             
@@ -60,7 +91,14 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
 
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pokemon.count
+        
+        if inSearchMode {
+            return filteredPokemon.count
+        } else {
+            return pokemon.count
+
+        }
+        
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -71,5 +109,35 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return CGSizeMake(105, 105)
     }
     
+    @IBAction func musicBtnPressed(sender: UIButton!) {
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 0.2 // dim the button a bit when the music is not playing
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0 // display the button normally when playing
+        }
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    // detect when text is etered in the searchBar
+    func  searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            view.endEditing(true) // close the keyboard
+            collection.reloadData()
+        } else {
+            inSearchMode = true
+            let lower = searchBar.text!.lowercaseString // grab the current text in the searchBar
+            filteredPokemon = pokemon.filter({$0.name.rangeOfString(lower) != nil}) // $0 grab an element from the array and put it in a variable and grab it's name property. For all Pokemon in thr array. Also rangeOfString checks if any of the name-string contain the string in the searchBar (lower). The closure is being run for every element
+            collection.reloadData() // refresh with the datasource
+        }
+    }
 }
+
+
+
 
